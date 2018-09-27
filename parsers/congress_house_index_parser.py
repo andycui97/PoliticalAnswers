2dbeb94f052e81eac8e4d67e260e38c43d877cb4
Andy Cui <andycui97@gmail.com>
2018-09-26 22:45:01 -0700

    basic parsing of congress data

--- parsers/congress_house_index_parser.py
+++ parsers/congress_house_index_parser.py
@@ -1,13 +1,15 @@
-from base_parser import simple_get, grab_body_highest_header
-import re
+from base_parser import *
 
+def clean_page(raw_html):
+    return remove_footer(remove_head(remove_header(clean_scripts(clean_forms(raw_html)))))
 
 def main():
     """
     For testing purposes only
     """
     raw_html = simple_get('https://www.congress.gov/roll-call-votes')
-    print(raw_html.decode("utf-8"))
+    raw_html_cleaned = clean_page(raw_html.decode("utf-8"))
+    print("\n".join(list_links(raw_html_cleaned)))
 
 if __name__== "__main__":
   main()
